9efde044d2d8ecc4d79b99ff9fc4b28995d8697c
Adam Baker <bakerac4@gmail.com>
2019-03-10 13:59:26 -0400

    Adding blueprint

--- blueprints/nativescript-glimmer-template/index.js
+++ blueprints/nativescript-glimmer-template/index.js
@@ -1,4 +1,5 @@
 'use strict';
+const chalk = require('chalk');
 
 module.exports = {
   description: '',
@@ -7,9 +8,35 @@ module.exports = {
 
   afterInstall: function() {
     return this.addPackagesToProject([
-      { name: 'nativescript-theme-core', target: '~1.0.4' },
-      { name: 'tns-core-modules', target: '5.2.2' },
-      { name: 'nativescript-glimmer', target: 'git+https://github.com/bakerac4/nativescript-glimmer.git#test3' }
+      { name: 'nativescript-theme-core', target: '~1.0.4'},
+      { name: 'tns-core-modules', target: '5.2.2'},
+      { name: 'nativescript-glimmer', target: 'git+https://github.com/bakerac4/nativescript-glimmer.git#test3'}
     ]); // is a promise
-  }
+  },
+  addPackagesToProject(packages, dev = false) {
+    let task = this.taskFor('npm-install');
+    let installText = (packages.length > 1) ? 'install packages' : 'install package';
+    let packageNames = [];
+    let packageArray = [];
+
+    for (let i = 0; i < packages.length; i++) {
+      packageNames.push(packages[i].name);
+
+      let packageNameAndVersion = packages[i].name;
+
+      if (packages[i].target) {
+        packageNameAndVersion += `@${packages[i].target}`;
+      }
+
+      packageArray.push(packageNameAndVersion);
+    }
+
+    this._writeStatusToUI(chalk.green, installText, packageNames.join(', '));
+
+    return task.run({
+      'save-dev': dev,
+      verbose: false,
+      packages: packageArray,
+    });
+  },
 };
