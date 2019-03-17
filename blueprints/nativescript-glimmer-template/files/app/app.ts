81bc8cfd8187163daab0e0791c30a7e8ce315689
Adam Baker <bakerac4@gmail.com>
2019-03-16 20:35:21 -0400

    Use package name

--- blueprints/nativescript-glimmer-template/files/app/app.ts
+++ blueprints/nativescript-glimmer-template/files/app/app.ts
@@ -1,17 +1,25 @@
-import NativescriptGlimmer from 'nativescript-glimmer';
-import { ResolverDelegate, Resolver, NativeComponent } from 'nativescript-glimmer';
-import Component from '@glimmer/component';
-import {knownFolders} from "tns-core-modules/file-system";
+import NativescriptGlimmer, { ResolverDelegate, Resolver } from 'nativescript-glimmer';
+import { knownFolders } from "tns-core-modules/file-system";
 const resolverDelegate = new ResolverDelegate();
 const resolver = new Resolver();
 
 function addTemplates(appFolder) {
-
     let templatesFile = appFolder.getFile("templates.json");
     let templates = templatesFile.readTextSync();
     // console.log(`Templates: ${templates}`);
     JSON.parse(templates).forEach(template => {
         resolverDelegate.addComponent(template.name, template.handle, template.source, template.capabilities);
+    });
+}
+
+function addComponents(appFolder) {
+    let componentsFile = appFolder.getFile("components.json");
+    let components = componentsFile.readTextSync();
+    console.log(`About to resolve require`);
+    JSON.parse(components).forEach(component => {
+        console.log(`About to resolve require`);
+        const classFile = require(`../src/ui/components/${component.name}/component.ts`);
+        resolver.addComponent(component.name, classFile.default);
     });
 }
 
@@ -21,20 +29,9 @@ requireAll(require.context('../src/ui/components/', true, /component.ts$/));
 try {
     let appFolder = knownFolders.currentApp();
     addTemplates(appFolder);
-
-    let componentsFile = appFolder.getFile("components.json");
-    let components = componentsFile.readTextSync();
-    console.log(`About to resolve require`);
-    JSON.parse(components).forEach(component => {
-        console.log(`About to resolve require`);
-        const classFile = require(`../src/ui/components/${component.name}/component.ts`);
-        resolver.addComponent(component.name, classFile.default);
-    });
-
-    const app = new NativescriptGlimmer('MainTemplate', {}, resolverDelegate, resolver);
-
+    addComponents(appFolder);
+    const app = new NativescriptGlimmer('<%= camelizedModuleName %>', {}, resolverDelegate, resolver);
     app.render();
-
 } catch(errors) {
-
+    console.log(errors);
 }
